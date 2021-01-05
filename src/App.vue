6e7d9b7213acba49f4511a1f3bcc544d584e859b
Tony <tony100new@gmail.com>
2021-01-05 19:42:17 +0800

    0105 commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="$style.app">
+    <div :class="$style.sidebar"></div>
+    <div :class="$style.body">
+      <div :class="$style.header"></div>
+      <div :class="$style.content">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" module>
+@import '@/styles/general/general.module.scss';
+.app {
+  @include block(100%, $background-color: #000); //width: 100%, height: auto;
+  margin: 0px;
+  .sidebar {
+    @include block(160px, 100%, $background-color: #000);
+    @include border();
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    z-index: 1;
+  }
+  .body {
+    @include block(100%);
+    .header {
+      height: 60px;
+      @include border();
+      margin-left: 160px;
+      margin-top: -10px;
+    }
+    .content {
+      @include border();
+      margin-left: 160px;
+      background-color: #000;
+    }
+  }
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/home/views/home-index'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes,
+})
+
+export default router
