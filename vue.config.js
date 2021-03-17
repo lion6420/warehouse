53082afac13de58462c7f964a93465d2f4d26fd1
ChenJianTing <tony100new@gmail.com>
2021-03-17 17:20:58 +0800

    modified production mode publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/warehouse/'
+    : '/',
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'test') {
       const scssRule = config.module.rule('scss');
