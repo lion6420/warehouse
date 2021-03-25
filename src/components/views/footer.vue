4c395b813df96833ca4b9ca14087c2233b1f6cc3
ChenJianTing <tony100new@gmail.com>
2021-03-25 19:02:47 +0800

    0325 commit

---
+++ src/components/views/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <content-footer></content-footer>
+</template>
+
+<script>
+import contentFooter from '@/components/utils/footer'
+export default {
+  components: {
+    contentFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
